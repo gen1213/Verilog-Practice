6270ca8d8acad8d1b4e96983c990bedc97820620
gen1213 <87006914+gen1213@users.noreply.github.com>
2021-07-06 16:10:15 +0800

    Update 095_Edgedetect.v

--- 2_Circuits/095_Edgedetect.v
+++ 2_Circuits/095_Edgedetect.v
@@ -3,18 +3,10 @@ module top_module (
     input [7:0] in,
     output [7:0] pedge
 );
-    integer i;
-    reg [7:0] in_tmp;
-    always @(posedge clk) begin
-        for(i = 0; i < 8; i = i + 1) begin
-            if(in_tmp[i] != in[i] && in[i] == 1) begin
-                pedge[i] = 1;
-            end
-            else begin
-                pedge[i] = 0;
-            end
-            in_tmp[i] = in[i];
-        end
+    reg [7:0]in_r;
+    always@(posedge clk)begin
+        in_r<=in;
+        pedge<=in&(~in_r);
     end
 
 endmodule
